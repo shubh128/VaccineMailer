7f5dc2562bb9e9b41ccce40117620832244ace65
Shubh Deep <shubhdeep41280@gmail.com>
2021-05-03 00:40:16 +0530

    Cleaned Code

--- notifier.js
+++ notifier.js
@@ -30,60 +30,7 @@ const smtpTransport = nodemailer.createTransport({
     },
   },
 });
-// const data = {
-//   sessions: [
-//     {
-//       center_id: 563020,
-//       name: "Delhi Cantt Gen Hosp. Site 1",
-//       state_name: "Delhi",
-//       district_name: "New Delhi",
-//       block_name: "Not Applicable",
-//       pincode: 110010,
-//       from: "09:00:00",
-//       to: "17:00:00",
-//       lat: 28,
-//       long: 77,
-//       fee_type: "Free",
-//       session_id: "566c852f-ae6c-4aa5-b476-ac756ea14986",
-//       date: "03-05-2021",
-//       available_capacity: 38,
-//       fee: "0",
-//       min_age_limit: 45,
-//       vaccine: "COVISHIELD",
-//       slots: [
-//         "09:00AM-11:00AM",
-//         "11:00AM-01:00PM",
-//         "01:00PM-03:00PM",
-//         "03:00PM-05:00PM",
-//       ],
-//     },
-//     {
-//       center_id: 576301,
-//       name: "Delhi Cantt Gen Hosp. Site 2",
-//       state_name: "Delhi",
-//       district_name: "New Delhi",
-//       block_name: "Not Applicable",
-//       pincode: 110010,
-//       from: "09:00:00",
-//       to: "17:00:00",
-//       lat: 28,
-//       long: 77,
-//       fee_type: "Free",
-//       session_id: "fd0ad122-c2f3-480b-ba35-ce766de2d7ba",
-//       date: "03-05-2021",
-//       available_capacity: 88,
-//       fee: "0",
-//       min_age_limit: 45,
-//       vaccine: "COVISHIELD",
-//       slots: [
-//         "09:00AM-11:00AM",
-//         "11:00AM-01:00PM",
-//         "01:00PM-03:00PM",
-//         "03:00PM-05:00PM",
-//       ],
-//     },
-//   ],
-// };
+
 exports.sendEmail = function (email, subjectLine, slotDetails, callback) {
   var html = "";
   slotDetails.map((slotDetail) => {
@@ -92,18 +39,8 @@ exports.sendEmail = function (email, subjectLine, slotDetails, callback) {
     str = str.replace(/,/g, "<br>");
     str += "<br><br><br>";
     html += str;
-    // console.log(typeof str);
   });
-  console.log("Html: ", html);
-  // console.log(typeof html);
 
-  let options = {
-    from: String("Vaccine Checker " + process.env.EMAIL),
-    to: email,
-    subject: subjectLine,
-    generateTextFromHTML: true,
-    html: "<h1>Vaccine Available, Details: </h1>" + slotDetails,
-  };
   const mailOptions = {
     from: String("Vaccine Checker " + process.env.EMAIL),
     to: email,
